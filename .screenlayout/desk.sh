b1b828172dc9263096c91d98baf9cf86c9698465
nooblet779 <bmkamunyu@gmail.com>
2020-11-30 17:19:08 +0300

    bak

--- .screenlayout/desk.sh
+++ .screenlayout/desk.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1368x768 --pos 1920x40 --rotate normal --output DP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
+xrandr --output eDP1 --primary --mode 1368x768 --pos 2044x0 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
